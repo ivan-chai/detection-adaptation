f9d541a0bfa88a7bb8076cc08019deec0e915876
Konstantin Sukharev <k.sukharev@gmail.com>
2020-11-20 12:33:15 +0500

    Add docstring

--- scripts/optimize_hyperparameters_domain_adaptation.py
+++ scripts/optimize_hyperparameters_domain_adaptation.py
@@ -1,3 +1,28 @@
+"""Script for hyperparameter optimization using optuna.
+
+All optimized hyperparameters in the config must be a dict
+with `parameter_space` and `parameter_space_arguments` keys.
+Only "categorical", "float" and "int" are valid values for `parameter_space` key.
+
+`parameter_space_arguments` is a dict with arguments,
+which passed to utility functions provided by :class:`optuna.trial.Trial`
+such as :meth:`optuna.trial.Trial.suggest_int`.
+
+https://optuna.readthedocs.io/en/stable/reference/generated/optuna.trial.Trial.html
+
+**Example**::
+
+    system:
+        subsystem:
+            parameter:
+                parameter_space: float
+                parameter_space_arguments:
+                    name: parameter_name
+                    low: 0
+                    high: 1
+                    step: 0.01
+"""
+
 import argparse
 import os
 
@@ -14,11 +39,9 @@ from dalib import datasets, models
 
 OPTIMIZE_KEY = "parameter_space"
 PARAMETER_SPACE_TO_TRIAL_METHOD = {
-    "cat": "suggest_categorical",
-    "int": "suggest_int",
-    "uniform": "suggest_uniform",
-    "loguniform": "suggest_loguniform",
-    "discrete_uniform": "suggest_discrete_uniform"
+    "categorical": "suggest_categorical",
+    "float": "suggest_float",
+    "int": "suggest_int"
 }
 
 
